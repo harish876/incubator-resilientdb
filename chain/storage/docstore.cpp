9c1adb112087a08670dbc034a686a038e146e3db
harish876 <harishgokul01@gmail.com>
2025-06-21 20:09:14 +0000

    refactor: adjust Bazel job count and enhance error handling in ResDocstoreDB methods

--- chain/storage/docstore.cpp
+++ chain/storage/docstore.cpp
@@ -79,7 +79,10 @@ int ResDocstoreDB::SetValue(const std::string& key,
   } else if (key == "INSERT") {
     handle_insert(serialized_document, s);
   } else {
-    handle_default_kv_store(key, serialized_document, s);
+    LOG(ERROR) << "Normal kv store case not handled" << key << s.ToString()
+               << " \n";
+    return -1;
+    // handle_default_kv_store(key, serialized_document, s);
   }
   if (s.ok()) {
     return 0;
@@ -140,18 +143,82 @@ void ResDocstoreDB::handle_default_kv_store(
 }
 
 std::string ResDocstoreDB::GetValue(const std::string& key) {
-  // std::string value;
+  LOG(ERROR) << "GetValue" << key << "\n";
+  nlohmann::json request;
+  auto result = nlohmann::json::parse(key, nullptr, true);
+  const std::string& collection_name = result.at("collection_name");
+  const std::string& filter_key = result.at("filter_key");
 
-  // leveldb::Status status = db_->Get(leveldb::ReadOptions(), key, &value);
-  // if (!status.ok()) {
-  //   value.clear();
-  // }
+  nlohmann::json metadata;
+  leveldb::Status s =
+      store_->CheckCollectionInRegistry(collection_name, metadata);
 
-  // return value;
-  return key;
+  if (!s.ok()) {
+    return "Collection not found";
+  }
+
+  if (!metadata.contains("options")) {
+    LOG(ERROR) << "Metadata missing 'options' field";
+    return "[]";
+  }
+
+  if (!metadata["options"].contains("secondary_key") ||
+      !metadata["options"].contains("primary_key")) {
+    LOG(ERROR)
+        << "'secondary_key' or 'primary_key' field not found in metadata";
+    return "[]";
+  }
+
+  const std::string& secondary_key = metadata.at("options").at("secondary_key");
+  const std::string& primary_key = metadata.at("options").at("primary_key");
+
+  std::string filter_value;
+  if (result.at("filter_value").is_string()) {
+    filter_value = result.at("filter_value").get<std::string>();
+  } else if (result.at("filter_value").is_number()) {
+    filter_value = std::to_string(result.at("filter_value").get<int64_t>());
+  } else {
+    LOG(ERROR) << "filter_value must be either string or number";
+    return "[]";
+  }
+
+  if (secondary_key == filter_key) {
+    std::vector<leveldb::SecondayKeyReturnVal> acc;
+    leveldb::Status s =
+        store_->GetSec(collection_name, filter_value, &acc, 1000);
+    if (!s.ok()) {
+      LOG(ERROR) << "Error at GetSec" << s.ToString() << " " << filter_value
+                 << "\n";
+      return "[]";
+    }
+    std::string values = "[";
+    bool first_iteration = true;
+    for (const auto& val : acc) {
+      if (!first_iteration) values.append(",");
+      first_iteration = false;
+      values.append(val.value);
+    }
+    values.append("]");
+    return values;
+
+    return "[]";
+  } else if (primary_key == filter_key) {
+    std::string value;
+    leveldb::Status s = store_->Get(collection_name, filter_value, value);
+    if (!s.ok()) {
+      LOG(ERROR) << "Error at GetSec" << s.ToString() << " " << filter_value
+                 << "\n";
+      return "[]";
+    }
+    return value;
+  } else {
+    return "Not implemented";
+  }
 }
 
 std::string ResDocstoreDB::GetAllValues(void) {
+  // Get All values from the database
+  std::vector<std::string> values;
   throw std::logic_error("Function not implemented");
 }
 
@@ -160,9 +227,102 @@ ResDocstoreDB::GetAllItems() {
   throw std::logic_error("Function not implemented");
 }
 
-std::string ResDocstoreDB::GetRange(const std::string& min_key,
-                                    const std::string& max_key) {
-  throw std::logic_error("Function not implemented");
+// TODO: Figure out a way to call this directly from the API layer.
+std::string ResDocstoreDB::GetRange(const std::string& key,
+                                    const std::string& dummy_key) {
+  LOG(ERROR) << "GetRange" << key << "\n";
+  nlohmann::json request;
+  auto result = nlohmann::json::parse(key, nullptr, true);
+  const std::string& collection_name = result.at("collection_name");
+  const std::string& filter_key = result.at("filter_key");
+
+  nlohmann::json metadata;
+  leveldb::Status s =
+      store_->CheckCollectionInRegistry(collection_name, metadata);
+
+  if (!s.ok()) {
+    return "Collection not found";
+  }
+
+  if (!metadata.contains("options")) {
+    LOG(ERROR) << "Metadata missing 'options' field";
+    return "[]";
+  }
+
+  if (!metadata["options"].contains("secondary_key") ||
+      !metadata["options"].contains("primary_key")) {
+    LOG(ERROR)
+        << "'secondary_key' or 'primary_key' field not found in metadata";
+    return "[]";
+  }
+
+  const std::string& secondary_key = metadata.at("options").at("secondary_key");
+  const std::string& primary_key = metadata.at("options").at("primary_key");
+
+  std::string filter_value_from;
+  if (result.at("filter_value_from").is_string()) {
+    filter_value_from = result.at("filter_value_from").get<std::string>();
+  } else if (result.at("filter_value_from").is_number()) {
+    filter_value_from =
+        std::to_string(result.at("filter_value_from").get<int64_t>());
+  } else {
+    LOG(ERROR) << "filter_value_from must be either string or number";
+    return "[]";
+  }
+
+  std::string filter_value_to;
+  if (result.at("filter_value_to").is_string()) {
+    filter_value_to = result.at("filter_value_to").get<std::string>();
+  } else if (result.at("filter_value_to").is_number()) {
+    filter_value_to =
+        std::to_string(result.at("filter_value_to").get<int64_t>());
+  } else {
+    LOG(ERROR) << "filter_value_to must be either string or number";
+    return "[]";
+  }
+
+  if (secondary_key == filter_key) {
+    std::vector<leveldb::SecondayKeyReturnVal> acc;
+    leveldb::Status s = store_->RangeGetSec(collection_name, filter_value_from,
+                                            filter_value_to, &acc, 1000);
+    if (!s.ok()) {
+      LOG(ERROR) << "Error at RangeGetSec" << s.ToString() << " "
+                 << filter_value_from << " " << filter_value_to << "\n";
+      return "[]";
+    }
+    std::string values = "[";
+    bool first_iteration = true;
+    for (const auto& val : acc) {
+      if (!first_iteration) values.append(",");
+      first_iteration = false;
+      values.append(val.value);
+    }
+    values.append("]");
+    return values;
+
+    return "[]";
+  } else if (primary_key == filter_key) {
+    vector<nlohmann::json> acc;
+    leveldb::Status s = store_->GetRange(collection_name, filter_value_from,
+                                         filter_value_to, acc);
+    if (!s.ok()) {
+      LOG(ERROR) << "Error at RangeGetSec" << s.ToString() << " "
+                 << filter_value_from << " " << filter_value_to << "\n";
+      return "[]";
+    }
+
+    std::string values = "[";
+    bool first_iteration = true;
+    for (const auto& doc : acc) {
+      if (!first_iteration) values.append(",");
+      first_iteration = false;
+      values.append(doc.dump(1, ' '));
+    }
+    values.append("]");
+    return values;
+  } else {
+    return "Not implemented";
+  }
 }
 
 bool ResDocstoreDB::UpdateMetrics() { return false; }
