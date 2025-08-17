25942e280661d2e6f59ca76a6a48f902ff9a57a0
harish876 <harish876@users.noreply.github.com>
2025-08-17 21:55:50 +0000

    deploy: 3b506129c7275ebf09aa338030480ef22d33896a

---
+++ navtreedata.js
@@ -0,0 +1,88 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "ResilientDB", "index.html", [
+    [ "Namespaces", "namespaces.html", [
+      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", "namespacemembers_dup" ],
+        [ "Functions", "namespacemembers_func.html", null ],
+        [ "Variables", "namespacemembers_vars.html", null ],
+        [ "Typedefs", "namespacemembers_type.html", null ],
+        [ "Enumerations", "namespacemembers_enum.html", null ],
+        [ "Enumerator", "namespacemembers_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", "functions_vars" ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", null ],
+        [ "Functions", "globals_func.html", null ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Macros", "globals_defs.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"d1/d44/classresdb_1_1Recovery.html#a16d630355aca59e4cf89d258939d761e",
+"d2/d02/resdb__txn__accessor_8h_source.html",
+"d3/d56/namespaceresdb_1_1common.html#a3fa16a6ab86b7d4bb7df6274807b9cf0",
+"d4/d2d/classresdb_1_1ServiceNetwork.html#ac1c908c92c1c6386bee2a35b64f982cf",
+"d5/d02/classresdb__driver_1_1driver_1_1TransactionsEndpoint.html",
+"d5/d3e/classresdb_1_1LRUCache.html",
+"d6/d49/certificate__tools__test_8cpp.html#a3c04138a5bfe5d72780bb7e82a18e627",
+"d7/d70/namespaceresilient__python__cache_1_1cache.html#a2a0b5bbf24c2ed4a06ef674c8a68945c",
+"d8/dcb/classresdb_1_1MockCheckPoint.html",
+"da/d19/classresdb_1_1ConsensusManager.html#a6ee5d408e5db8134290afa5dbe162568",
+"da/de5/hash_8cpp_source.html",
+"db/deb/classresdb_1_1common_1_1Consensus.html#aae02ac41fa631ef21db79090eb1ff8c3",
+"dc/db0/classresdb_1_1CheckPointManager.html#a01a5417fd7d2d4f1a3b2da681d3c0e11",
+"dd/d68/mock__geo__global__executor_8h_source.html",
+"de/d3b/namespacetest__driver.html#a1d693f578ee6aaf581b83243decf11c5",
+"de/df1/classresdb__driver_1_1exceptions_1_1KeypairMismatchException.html",
+"df/ddc/classresdb_1_1utxo_1_1Wallet.html",
+"namespaces.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
